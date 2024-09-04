66cf46c1da8eedfac58c838bce69080cb695eae2
dangdinhtai <dangdinhtai0001@gmail.com>
2024-09-04 18:30:14 +0700

    chore:

--- src/views/identity-access/users/index.vue
+++ src/views/identity-access/users/index.vue
@@ -6,16 +6,19 @@
       class="ag-theme-friday"
       :columnDefs="columnDefs"
       :rowData="rowData"
+      :defaultColDef="defaultColDef"
+      :rowClass="rowClass"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed } from 'vue'
 import 'ag-grid-community/styles/ag-grid.css' // Mandatory CSS required by the Data Grid
 // import 'ag-grid-community/styles/ag-theme-alpine.css' // Optional Theme applied to the Data Grid
 import '@/assets/styles/ag-theme-friday.css'
 import { AgGridVue } from 'ag-grid-vue3'
+import { type ColDef } from 'ag-grid-community'
 
 const columnDefs = [
   { headerName: 'Make', field: 'make' },
@@ -27,4 +30,12 @@ const rowData = [
   { make: 'Ford', model: 'Mondeo', price: 32000 },
   { make: 'Porsche', model: 'Boxter', price: 72000 }
 ]
+
+const defaultColDef: ColDef = {
+  cellClass: ['friday-default-cell']
+}
+
+const rowClass = computed<string | string[]>(() => {
+  return ['friday-default-row']
+})
 </script>
