ce5744e0fb9e1cc882b52fe74883328dfd8c4296
dangdinhtai <dangdinhtai0001@gmail.com>
2024-09-16 19:11:31 +0700

    feat: init range calender

--- src/views/identity-access/home/index.vue
+++ src/views/identity-access/home/index.vue
@@ -64,6 +64,8 @@
         </form.Field>
       </div>
     </form>
+    <RangeCalendar v-model="value" class="rounded-md border" />
+    {{ value }}
   </div>
 </template>
 
@@ -72,6 +74,19 @@ import { useForm } from '@tanstack/vue-form'
 import { FormItem } from '@/components/molecules/form'
 import { Input } from '@/components/atoms/ui/input'
 
+import { type Ref, ref } from 'vue'
+import type { DateRange } from 'radix-vue'
+import { getLocalTimeZone, today } from '@internationalized/date'
+import { RangeCalendar } from '@/components/atoms/ui/range-calendar'
+
+const start = today(getLocalTimeZone())
+const end = start.add({ days: 7 })
+
+const value = ref({
+  start,
+  end
+}) as Ref<DateRange>
+// ----------------------------------
 const form = useForm({
   defaultValues: {
     firstName: '',
